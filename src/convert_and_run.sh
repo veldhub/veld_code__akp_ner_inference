4c77426436687cba69323ebd66aefb7b3aa280aa
steffres <stefan.resch@oeaw.ac.at>
2023-10-18 21:31:18 +0200

    better fetch and incremental parsing+persisting

---
+++ src/convert_and_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# for execution without jupyter and just in a terminal, this script converts the .ipynb file to a 
+# .py and launches it
+jupyter nbconvert --to script load_and_apply_ner.ipynb
+python load_and_apply_ner.py
